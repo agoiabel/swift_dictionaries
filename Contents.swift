19dcc446c5f1153c79f8c71bd06a34dc5dc19b52
Abel Agoi Adeyemi <agoiabeladeyemi@gmail.com>
2018-01-05 23:27:11 +0100

    dealing with dictionaries

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+//: Will use this playground to learn dictionaries
+
+//declaring a dictionary
+var my_dictionary = [
+    "01": "First Item",
+    "02": "Second Item",
+    "03": "Third Item",
+    "04": "Four Item",
+    "05": "Fifth Item"
+]
+
+//modify an item in a dictionary
+my_dictionary["01"] = "Another Item"
+
+//inserting another item into the dictionary
+my_dictionary["06"] = "Sixth Item"
+
+//removing an item from a dictionary using nill
+my_dictionary["06"] = nil
+
+//removing an item from a dictionary using forKey
+my_dictionary.removeValue(forKey: "05")
+
+//view the whole item in the dictionary
+my_dictionary
+
+
+
+
+
